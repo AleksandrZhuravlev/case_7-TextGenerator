f3cc6ec5dde66ca7f7ff77181917155d819502c6
nearlyoff <kremlin.vova2@gmail.com>
2021-04-11 23:47:30 +0700

    ready

--- main.py
+++ main.py
@@ -1,49 +1,92 @@
-# Case 7
+# Case-Study #7 "Text Generator"
 # Programmers:
 # Alexander Zhuravlev (75%)
-# test
+# Kremlin V. (25%)
 
 import random
 
-text = []
-keys = []
-connections = {}
-start_words = []
-with open('input.txt', 'r', encoding='UTF-8') as inp:
-    for stroka in inp:
-        for word in stroka.split():
-            text.append(word)
-            if word[0].isupper() == True:
-                start_words.append(word)
 
-for i in text:
-    for j in range(len(text)):
-        if text[j] == i:
-            try:
-                keys.append(text[j + 1])
-            except:
-                pass
-    connections.update({i: keys})
+def control():
+    """
+    Function, checking input data.
+    """
+    try:
+        n = int(input('Amount of sentences to generate: '))
+        return n
+    except ValueError:
+        print('Integer number from 1 to 100 required. Please, try again.')
+        exit()
+
+
+def lists(filename):
+    """
+    Function, making lists and return them to main().
+    """
+    text = []
     keys = []
+    connections = {}
+    start_words = []
+    try:
+        with open(filename, 'r', encoding='UTF-8') as infile:
+            for line in infile:
+                for word in line.split():
+                    text.append(word)
+                    if word[0].isupper():
+                        start_words.append(word)
+        return text, keys, connections, start_words
+    except FileNotFoundError:
+        print('File "' + filename + '" was not found. Please, try again.')
+        exit()
 
 
-def sentence():
-    '''
-    function generates sentence
-    with lenght from 5 to 20
-    '''
-    sent = ''
-    length = random.randint(5, 20)
-    prev = random.choice(start_words)
-    sent += prev
-    for i in range(length):
-        next = random.choice(connections[prev])
-        prev = next
-        sent += ' ' + prev
-    sent += '.'
-    print(sent)
-    return ''
+def check(text, keys, connections):
+    for word in text:
+        for j in range(len(text)):
+            if text[j] == word:
+                try:
+                    keys.append(text[j + 1])
+                except IndexError:
+                    pass
+        connections.update({word: keys})
 
 
-for i in range(10):
-    print(sentence())
+def generator(start_words, connections):
+    """
+    Function, generates sentence with length from 5 to 20.
+    """
+    try:
+        sentence = ''
+        length = random.randint(5, 20)
+        previous = random.choice(start_words)
+        sentence += previous
+        for k in range(length):
+            sequent = random.choice(connections[previous])
+            previous = sequent
+            sentence += ' ' + previous
+        sentence += '.'
+        print(sentence)
+        return ''
+    except IndexError:
+        return ''
+
+
+def output(n, start_words, connections):
+    """
+    Function for data output.
+    """
+    for i in range(n):
+        print(generator(start_words, connections))
+
+
+def main():
+    """
+    Main Function with all other functions.
+    """
+    n = control()
+    filename = input('Filename: ')
+    text, keys, connections, start_words = lists(filename)
+    check(text, keys, connections)
+    output(n, start_words, connections)
+
+
+main()
